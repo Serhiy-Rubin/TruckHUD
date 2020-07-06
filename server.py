ab4d7019da44a9fe6428c38a8d307d7f051627c0
qrlk <hi@qrlk.me>
2020-07-06 17:29:54 +0300

    Sanic server, added 842 request (all users on a server)

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-from aiohttp import web
+from sanic import Sanic
+from sanic.response import json
+from sanic.response import text
+from sanic.exceptions import NotFound
 from datetime import datetime
 import urllib.parse
 import time
 import linecache
-import json
+import json as js
 import sys
+
 
 try:
     sys.argv[1]
@@ -17,9 +21,12 @@ except IndexError:
 DELAY = int(sys.argv[1])
 
 data = {}
-async def handle(request):
+app = Sanic()
+
+@app.exception(NotFound)
+async def handle(request, exception):
     timer = time.time()
-    info = json.loads(urllib.parse.unquote(request.path.replace("/","")))
+    info = js.loads(urllib.parse.unquote(request.url[27:]))
 
     if info:
         if info["request"] == 1:
@@ -42,23 +49,29 @@ async def handle(request):
             data[ip]["senders"][sender]["data"] = info["info"]
 
             if pair_name == "____":
-                return web.Response(text=json.dumps({"result": "gotu"}))
+                print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Подготовлен ответ для {sender}. Напарника нет {pair_name}. Отправлена заглушка. Обработка заняла: {(time.time()-timer):.6f} с")
+                return json({"result": "gotu"})
 
             if pair_name in data[ip]["senders"]:
                 if data[ip]["senders"][pair_name]["data"]["pair_mode_name"] == sender:
-                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Подготовлен ответ для {sender}. Он спарен с {pair_name}. Отправлены данные: {json.dumps(data[ip]['senders'][pair_name])} Обработка заняла: {(time.time()-timer):.6f} с")
-                    return web.Response(text=json.dumps({"result": "ok", "timestamp": time.time(), "data": data[ip]['senders'][pair_name], "delay": DELAY}))
+                    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Подготовлен ответ для {sender}. Он спарен с {pair_name}. Отправлены данные: {js.dumps(data[ip]['senders'][pair_name])} Обработка заняла: {(time.time()-timer):.6f} с")
+                    return json({"result": "ok", "timestamp": time.time(), "data": data[ip]['senders'][pair_name], "delay": DELAY})
                 else:
                     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Подготовлен ответ для {sender}. Он хочет спариться с {pair_name}, а {pair_name} уже спаривается с {data[ip]['senders'][pair_name]['data']['pair_mode_name']}. Обработка заняла: {(time.time()-timer):.6f} с")
-                    return web.Response(text=json.dumps({"result": "error", "timestamp": time.time(), "reason": 403, "delay": DELAY}))
+                    return json({"result": "error", "timestamp": time.time(), "reason": 403, "delay": DELAY})
             else:
                 print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S || ')}[{ip}]: Подготовлен ответ для {sender}. Он хочет спариться с {pair_name}, но {pair_name} не найден в словаре сервера. Обработка заняла: {(time.time()-timer):.6f} с")
-                return web.Response(text=json.dumps({"result": "error", "timestamp": time.time(), "reason": 404, "delay": DELAY}))
-      
-    return web.Response(text='error')
+                return json({"result": "error", "timestamp": time.time(), "reason": 404, "delay": DELAY})
+        if info["request"] == 843:
+            if info["server"] in data:
+                temp = data[info["server"]]
+                for item in temp["senders"].keys():
+                    temp["senders"][item]["data"].pop("pos", None)
+                return json({"data": temp})
+            else:
+                return text('error')
+    return text('error')
 
 
-app = web.Application()
-app.router.add_get('/{name}', handle)
-web.run_app(app, host = '0.0.0.0', port=43136)
-
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=43136, access_log=False)
